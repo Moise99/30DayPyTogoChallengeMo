f9f439ba418ce6e4493105b17b7c14ec68590442
Moise99 <38433943+Moise99@users.noreply.github.com>
2025-08-06 22:56:26 +0000

    day15

---
+++ day15/exercises.py
@@ -0,0 +1,36 @@
+# Example of SyntaxError
+# print("Hello, World!"
+      
+# Example of NameError
+# print("Hello, " + name)
+
+# Example of IndexError
+# my_list = [1, 2, 3]
+# print(my_list[5])
+
+
+# Example of ModuleNotFoundError
+# import non_existent_module
+
+
+# Example of AttributeError
+# my_string = "Hello"
+# print(my_string.non_existent_method())
+
+# Example of KeyError
+# my_dict = {"a": 1, "b": 2}
+# print(my_dict["c"])
+
+# Example of TypeError
+# my_number = 10
+# print(my_number + "5")
+
+
+# Example of ValueError
+# my_number = int("not a number")
+# print(my_number)
+
+
+# Example of ZeroDivisionError
+# my_number = 10
+# print(my_number / 0)
