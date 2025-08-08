074820028cd8d26d9da64ccd4f6c13213c5a8d13
Moise99 <38433943+Moise99@users.noreply.github.com>
2025-08-08 22:22:06 +0000

    day17

---
+++ day17/exercises.py
@@ -0,0 +1,10 @@
+#1. Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+nordic_countries = names[:5]
+es = names[5]
+ru = names[6]
+
+print(nordic_countries)  
+print(es)
+print(ru)
